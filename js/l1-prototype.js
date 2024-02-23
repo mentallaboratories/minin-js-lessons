6a69c1fc31752ad5328635c4063e70c7988ecae6
mentallaboratories <93845562+mentallaboratories@users.noreply.github.com>
2024-02-23 23:17:25 +0200

    prototype lesson done

--- js/l1-prototype.js
+++ js/l1-prototype.js
@@ -1,7 +1,23 @@
-const person = {
+// const person = {
+//     name: 'John',
+//     age: 21,
+//     greet: function(){
+//         console.log('Hi, mazafaka!')
+//     }
+// }
+
+
+const person = new Object({
     name: 'John',
     age: 21,
     greet: function(){
-        console.log('Hi, mazafaka!')
+    console.log('Hi, mazafaka!')
     }
+})
+
+Object.prototype.sayHello = function(){
+    console.log('Hello,World!))')
 }
+
+const jane = Object.create(person)
+jane.name = 'Jane'
